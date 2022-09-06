0ca9ff6ba6de5ba64504d667a36a8901faacc52b
Tatyana Dmitrieva <visyar76@gmail.com>
2022-09-06 13:26:56 +0300

    Added tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,98 @@
 package ru.netology.service;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
-public class CashbackHackServiceTest extends TestCase {
+import static org.junit.Assert.assertEquals;
 
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldReturnCashback1With999() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1;
+        int actual = service.remain(999);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnCashbackZeroWith1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnCashback999With1001() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 999;
+        int actual = service.remain(1001);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnCashback1001() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1001;
+        int actual = service.remain(-1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnCashback1000With0() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1000;
+        int actual = service.remain(0);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnCashback999With1() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 999;
+        int actual = service.remain(1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnCashbackZeroWith2000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(2000);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnCashback50With950() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 950;
+        int actual = service.remain(50);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnCashback445With555() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 445;
+        int actual = service.remain(555);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnCashback1With1999() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1;
+        int actual = service.remain(1999);
+        assertEquals(expected, actual);
+    }
 }
